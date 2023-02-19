b68a08a1cb9991e2901da2f28cf14623e025c9c1
gtway <github@gettrueway.ru>
2023-02-19 03:47:20 +0300

    Add the Flatpak upgrade

--- update.sh
+++ update.sh
@@ -3,10 +3,13 @@
 # The easy script for terminal command 
 # [sudo apt update && sudo apt dist-upgrade && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean]
 
-printf "\033[32m\nFetch the fresh packages from reposirories\033[0m \n\n"
+printf "\033[32m\nFetch the fresh packages from reposirories\033[0m \n"
 sudo apt update 
 
 forUpdate=`sudo /usr/lib/update-notifier/update-motd-updates-available`
+isFlatpakInstalled=`dpkg-query -W flatpak`
+flatpakRegexp="^(flatpak*)"
+
 if [ -n "${forUpdate}" ]; then
     printf "\033[32mUpdate has been completed. Look at the packages for upgrade!\033[0m \n\n"
     printf "\033[32m" && sudo apt list --upgradable && printf "\033[0m \n\n" 
@@ -17,14 +20,20 @@ if [ -n "${forUpdate}" ]; then
     printf "\033[32mThe dist-upgrade has been completed. Start packages upgrade.\033[0m \n\n"
     sudo apt upgrade -y
 
-    printf "\033[32mUpgrade has been completed. Start the clear.\033[0m \n\n"
-
-    sudo apt -y autoremove --purge
-    sudo apt -y autoclean
-    printf "\033[32mAutoremove and autoclean has been completed.\033[0m \n"
+    printf "\033[32mUpgrade has been completed.\n"
 fi
 
-printf "\n\033[32mEverything was updated. Enjoy it!\033[0m \n\n"
+if [[ ${isFlatpakInstalled}=~$flatpakRegexp ]]; then
+    printf "\n\033[32mUpgrade the Flatpak package manager.\033[0m \n"
+    sudo flatpak update
+fi
+
+printf "\033[32m\nClear the system packages.\033[0m \n"
+sudo apt -y autoremove --purge
+sudo apt -y autoclean
+printf "\033[32mAutoremove and autoclean has been completed.\033[0m \n\n"
+
+printf "\033[32mEverything was updated. Enjoy it!\033[0m \n\n"
 
 if [ -f /var/run/reboot-required ]; then
     printf "\033[31mPlease, reboot your system, it's required!\033[0m \n\n"
